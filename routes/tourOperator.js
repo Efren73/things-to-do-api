69fdf3b52f3e2ba5689d0eada7bd929866fe9782
MariaFernanda2502 <ferchasalvador25@gmail.com>
2022-05-26 17:51:40 -0500

    Peticiones create

--- routes/tourOperator.js
+++ routes/tourOperator.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const admin = require ('firebase-admin')
 const db = require('../firebase')
+
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
   const cityRef = db.collection('TOUR').doc('LwCvn4yfT91Y4ekEaGlh');
@@ -15,5 +16,43 @@ router.get('/', async function(req, res, next) {
 res.send('respond with a resource');
 });
 
+/* CREATE TOUR -------------------------------------------------- */
+router.get("/create-tour", async(req, res, next) => {
+  try {
+    const data = {
+    };
+
+    // Agrega nuevo documento y deja que firestore cree la clave
+    const newTour = await db.collection('TOUR').add(data);
+    res.send("ok");
+
+  } catch (error) {
+    next();
+  }
+});
+
+
+/* CREATE TOUR OPERADOR ----------------------------------------- */
+router.post("/create-tour-operador", async(req, res, next) => {
+  try {
+    const data = req.body;
+
+    // Agrega nuevo documento y deja que firestore cree la clave
+    const newTour = await db.collection('PET_OWNER').add(data);
+    res.send("ok");
+
+  } catch (error) {
+    next();
+  }
+});
+
+/* UPDATE TOUR -------------------------------------------------- */
+router.update("update-tour", async(req, res, next) => {
+  try {
+    const data = req.body;
+
+
+  }
+});
 
 module.exports = router;
