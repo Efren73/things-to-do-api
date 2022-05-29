c63e95a354bc9f2ee874beb01a4ab9d71c6c5e0f
MariaFernanda2502 <ferchasalvador25@gmail.com>
2022-05-29 14:08:57 -0500

    Añadiendo update-tour-operator y update-admin, arreglos a los create, actualización de updatedAt, createdAd

--- routes/tourOperator.js
+++ routes/tourOperator.js
@@ -31,8 +31,8 @@ router.get('/one-tour/:idTour', async function(req, res, next) {
     console.log('No such document!');
   } else {
     console.log('Document data:', doc.data());
+    res.send(doc.data());
   }
-res.send(doc.data());
 });
 
 //Get tour operator info
@@ -50,65 +50,108 @@ router.get('/info/:idTourOperator', async function(req, res, next){
   }
 });
 
-/* CREATE TOUR -------------------------------------------------- */
-router.get("/create-tour", async(req, res, next) => {
-  try {
-    const data = {
-       /*
-      tourOperator:
-      tourOperatorName:
-      tourOperatorCountry:
-      createdAt:*/
-    };
+/* CREATE TOUR --------------------------------------- */
+router.post("/create-tour", async(req, res, next) => {
+  const data = req.body;
 
+  try {
     // Agrega nuevo documento y deja que firestore cree la clave
     const newTour = await db.collection('TOUR').add(data);
-    res.send("ok");
 
+    // Actualiza la fecha de creación
+    const createdAt = await newTour.update({
+      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    });
+
+    return res.status(201).json({
+      name: "Creación exitosa",
+      message: "Se realizo la creación del tour exitosamente"
+    });
   } catch (error) {
-    next();
+    next(error);
   }
 });
 
-
-/* CREATE TOUR OPERADOR ----------------------------------------- */
-router.post("/create-tour-operador", async(req, res, next) => {
+/* CREATE TOUR OPERADOR --------------------------------------- */
+router.post("/create-tour-operator", async(req, res, next) => {
+  const data = req.body; 
   try {
-    const data = req.body;
-
     // Agrega nuevo documento y deja que firestore cree la clave
     const newTour = await db.collection('TOUR_OPERATOR').add(data);
-    res.send("ok");
 
-  } catch (error) {
-    next();
+    // Actualiza la fecha de creación
+    const createdAt = await newTour.update({
+      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    });
+    
+    return res.status(201).json({
+      name: "Creación exitosa",
+      message: "Se realizo la creación del tour operador exitosamente"
+    });
+  } catch(error) {
+    next(error);
   }
 });
 
-/* UPDATE TOUR -------------------------------------------------- */
+/* UPDATE TOUR ----------------------------------------------- */
 router.put('/update-tour/:idTour', async (req, res, next) => {
   const { idTour } = req.params;
   const { body } = req;
   
   try {
+    // Confirmamos que existe
     const tourRef = db.collection('TOUR').doc(idTour);
     const doc = await tourRef.get();
     if (!doc.exists) {
       return res.status(404).json({
         name: "Not found",
-        message: "Sorry, el usuario que buscas no existe"
+        message: "Sorry, el tour que buscas no existe"
       })
     } else {
-      console.log('Document data:', body);
-      const tour = await db.collection('TOUR').doc(idTour).set(body);
-      /*
-      const timeUpdate = await tourRef.update({
-        updatedAt: FieldValue.serverTimestamp()
-      });*/
-        return res.status(200).json({
-          name: "Edicion exitosa",
-          message: "Se realizo la edición exitosamente"
-        })
+      // Actualiza el documento
+      const tour = await db.collection('TOUR').doc(idTour).update(body);
+
+      // Actualiza la fecha de actualización
+      const updateAt = await tourRef.update({
+        updateAt: admin.firestore.FieldValue.serverTimestamp()
+      });
+
+      return res.status(200).json({
+        name: "Edicion exitosa",
+        message: "Se realizo la edición del tour exitosamente"
+      });
+  }} catch(err) {
+    next(err);
+  }
+});
+
+/* UPDATE TOUR OPERADOR ------------------------------------------------------- */
+router.put('/update-tour-operator/:idTourOperator', async (req, res, next) => {
+  const { idTourOperator } = req.params;
+  const { body } = req;
+  
+  try {
+    // Confirmamos que existe
+    const tourOperatorRef = db.collection('TOUR_OPERATOR').doc(idTourOperator);
+    const doc = await tourOperatorRef.get();
+    if (!doc.exists) {
+      return res.status(404).json({
+        name: "Not found",
+        message: "Sorry, el tour operador que buscas no existe"
+      })
+    } else {
+      // Actualiza el documento
+      const tour = await db.collection('TOUR_OPERATOR').doc(idTourOperator).update(body);
+
+      // Actualiza la fecha de actualización
+      const updateAt = await tourOperatorRef.update({
+        updateAt: admin.firestore.FieldValue.serverTimestamp()
+      });
+
+      return res.status(200).json({
+        name: "Edicion exitosa",
+        message: "Se realizo la edición del tour operador exitosamente"
+      });
   }} catch(err) {
     next(err);
   }
