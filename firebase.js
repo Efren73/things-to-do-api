8fd7c0fd5c6f29a5cb2b994f703965bd1097420d
MariaFernanda2502 <ferchasalvador25@gmail.com>
2022-05-30 18:04:31 -0500

    API things to do

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./llave.json');
+
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY)
 
 module.exports = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-
 })
 
 const db = admin.firestore();
